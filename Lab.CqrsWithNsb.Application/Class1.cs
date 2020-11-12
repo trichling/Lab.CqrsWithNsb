771429f542ae7c4c89ea0c4a0e148812e969f7fa
Tobias Richling <RichlinT@apetito.com>
2020-11-12 10:11:34 +0100

    Initial

---
+++ Lab.CqrsWithNsb.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lab.CqrsWithNsb.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lab.CqrsWithNsb.Endpoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab.CqrsWithNsb.Endpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
